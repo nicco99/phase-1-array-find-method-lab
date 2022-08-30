59312db1217fdd418051fb5841e2e77489294bc7
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-30 15:59:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(record) {
+  let win = record.find(value => value.result==="W")
+  if(win) {
+    return win.year;
+  } else {
+    return win
+  }
+  
+  
+}
+
